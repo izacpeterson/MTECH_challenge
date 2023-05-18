2d2e4343f92b64cbaceaff2e39593a896c917eb4
izacpeterson <izacpeterson@gmail.com>
2023-05-18 02:01:11 -0600

    edit comment

--- app.js
+++ app.js
@@ -16,11 +16,34 @@ class Comment {
 let posts = [];
 
 posts.push(new Post("Trying to decide a career path? Programming is the move. Change my mind", "WhySoSerious45"));
+posts[0].comments.push(new Comment("I agree", "TheJoker"));
+posts[0].comments.push(new Comment("I disagree", "Batman"));
 
 function addComment(postIndex) {
   let author = document.getElementById("new-comment-user").value;
   let comment = document.getElementById("new-comment-text").value;
   posts[postIndex].comments.push(new Comment(comment, author));
+  render();
+}
+function editComment(commentIndex) {
+  let commentHTML = document.getElementById(`comment-${commentIndex}`);
+  let comment = posts[0].comments[commentIndex];
+  commentHTML.innerHTML += `
+    <div class="edit-comment">
+        <input type="text" class="edit-comment-text" id="edit-comment-text" value="${comment.body}">
+        <button class="edit-comment-submit" onClick="saveComment(${commentIndex})">Save</button>
+    </div>
+    `;
+
+  let commentActionsHTML = document.getElementById(`comment-${commentIndex}`).getElementsByClassName("comment-actions")[0];
+  commentActionsHTML.innerHTML = `
+        <div class="comment-action" onClick="render()">Cancel</div>
+    `;
+}
+
+function saveComment(commentIndex) {
+  let newComment = document.getElementById("edit-comment-text").value;
+  posts[0].comments[commentIndex].body = newComment;
   render();
 }
 
@@ -48,20 +71,22 @@ function render() {
     let comments = document.getElementById("comments");
     post.comments.forEach((comment, index) => {
       comments.innerHTML += `
-                    <div class="comment">
+                <div class="comment" id="comment-${index}">   
+                    <div class="comment-content">
                         <div class="user-image">
                             <img src="./images/user.png" alt="user image">
                         </div>
-                        <div class="comment-content">
+                        <div class="comment-details">
                             <div class="comment-author">${comment.author}</div>
                             <div class="comment-body">${comment.body}</div>
-                            <div>${index}</div>
                         </div>
                         <div class="comment-actions">
-                            <div class="comment-action">Edit</div>
+                            <div class="comment-action" onClick="editComment(${index})">Edit</div>
                             <div class="comment-action">Delete</div>
                         </div>
                     </div>
+
+                </div>
                 `;
     });
   });
