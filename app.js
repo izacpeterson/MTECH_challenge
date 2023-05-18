ddf417613e755c16e1c712570e2adce14c7e124c
izacpeterson <izacpeterson@gmail.com>
2023-05-18 04:09:45 -0600

    added proper post object references.

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-//MTECH challenge app
+//MTECH challenge app. Probably overworked it quite a bit, but I like how it turned out.
 
 //Classes. Probably don't need a post class, as there will only be one post. But I'm going to make one anyway.
 class Post {
-  constructor(body, author) {
+  constructor(body, author, id) {
     this.body = body;
     this.author = author;
     this.comments = [];
+    this.id = id;
   }
   addComment() {
-    let author = document.getElementById("new-comment-user").value;
-    let comment = document.getElementById("new-comment-text").value;
+    let author = document.getElementById(`new-comment-user-${this.id}`).value;
+    let comment = document.getElementById(`new-comment-text-${this.id}`).value;
 
-    this.comments.push(new Comment(comment, author, this.comments.length));
-
+    // this.comments.push(new Comment(comment, author, this.comments.length, this.id));
+    this.comments.unshift(new Comment(comment, author, this.comments.length, this.id));
     render();
+  }
+  fixCommentIndex() {
+    //fixes the id of the comments array due to unshift and splice
+    this.comments.forEach((comment, index) => {
+      comment.id = index;
+    });
   }
 }
 
 class Comment {
-  constructor(body, author, index) {
+  constructor(body, author, index, postID) {
     this.body = body;
     this.author = author;
     this.id = index;
+    this.postID = postID;
   }
 
-  edit(postIndex, commentIndex) {
+  edit() {
     render();
-    let commentHTML = document.getElementById(`comment-${this.id}`);
-    let comment = posts[0].comments[this.id];
+    console.log(this.id);
+    let commentHTML = document.getElementById(`comment-${this.postID}-${this.id}`);
     commentHTML.innerHTML += `
       <div class="edit-comment">
-          <input type="text" class="edit-comment-text" id="edit-comment-text" value="${comment.body}">
-          <button class="edit-comment-submit" onClick="posts[${postIndex}].comments[${commentIndex}].save()">Save</button>
+          <input type="text" class="edit-comment-text" id="edit-comment-text" value="${this.body}">
+          <button class="edit-comment-submit" onClick="posts[${this.postID}].comments[${this.id}].save()">Save</button>
       </div>
       `;
 
-    let commentActionsHTML = document.getElementById(`comment-${this.id}`).getElementsByClassName("comment-actions")[0];
+    let commentActionsHTML = document.getElementById(`comment-${this.postID}-${this.id}`).getElementsByClassName("comment-actions")[0];
     commentActionsHTML.innerHTML = `
           <div class="comment-action" onClick="render()">Cancel</div>
       `;
@@ -46,16 +54,18 @@ class Comment {
     render();
   }
   delete() {
-    posts[0].comments.splice(this.id, 1);
+    let index = posts[this.postID].comments.indexOf(this);
+    posts[this.postID].comments.splice(index, 1);
     render();
   }
 }
 
 // The render function. Used to render out every post and comment. Also used to 'cancel' editing by re-rendering the page.
 function render() {
-  localStorage.setItem("posts", JSON.stringify(posts));
   let feed = document.getElementById("feed");
   feed.innerHTML = "";
+
+  localStorage.setItem("posts", JSON.stringify(posts));
 
   // Manipulating the DOM twice might not be best, might rework later.
   posts.forEach((post, postIndex) => {
@@ -68,18 +78,20 @@ function render() {
                 ${post.body}
             </div>
         </div>
-        <div class="post-comments" id="comments">
+        <div class="post-comments" id="comments-${postIndex}">
         </div>
         <div class="add-comment">
-            <input type="text" class="new-comment-user" id="new-comment-user" placeholder="Display Name">
-            <input type="text" class="new-comment-text" id="new-comment-text" placeholder="Add a comment...">
+            <input type="text" class="new-comment-user" id="new-comment-user-${postIndex}" placeholder="Display Name">
+            <input type="text" class="new-comment-text" id="new-comment-text-${postIndex}" placeholder="Add a comment...">
             <button class="new-comment-submit" onClick="posts[${postIndex}].addComment()">Submit</button>
         </div>
+        <pre>${JSON.stringify(posts, null, 2)}</pre>
+
     `;
-    let comments = document.getElementById("comments");
+    let comments = document.getElementById(`comments-${postIndex}`);
     post.comments.forEach((comment, commentIndex) => {
       comments.innerHTML += `
-        <div class="comment" id="comment-${commentIndex}">   
+        <div class="comment" id="comment-${postIndex}-${commentIndex}">   
             <div class="comment-content">
                 <div class="user-image">
                     <img src="./images/user.png" alt="user image">
@@ -89,7 +101,7 @@ function render() {
                     <div class="comment-body">${comment.body}</div>
                 </div>
                 <div class="comment-actions">
-                    <div class="comment-action" onClick="posts[${postIndex}].comments[${commentIndex}].edit(${postIndex},${commentIndex})">Edit</div>
+                    <div class="comment-action" onClick="posts[${postIndex}].comments[${commentIndex}].edit()">Edit</div>
                     <div class="comment-action" onClick="posts[${postIndex}].comments[${commentIndex}].delete()">Delete</div>
                 </div>
             </div>
@@ -99,13 +111,22 @@ function render() {
   });
 }
 
-// If there are posts in local storage, load them. Otherwise, render the page.
-// if (localStorage.getItem("posts")) {
-//   posts = JSON.parse(localStorage.getItem("posts"));
-// }
 let posts = [];
 
-posts.push(new Post("Trying to decide a career path? Programming is the move. Change my mind", "WhySoSerious45"));
-posts[0].comments = [new Comment("I agree", "John", 0), new Comment("I disagree", "Jane", 1), new Comment("I'm not sure", "Joe", 2)];
+posts.push(new Post("Trying to decide a career path? Programming is the move. Change my mind", "WhySoSerious45", 0));
+posts[0].comments = [new Comment("I agree", "John", 0, 0), new Comment("I disagree", "Jane", 1, 0), new Comment("I'm not sure", "Joe", 2, 0)];
+
+if (localStorage.getItem("posts")) {
+  posts = [];
+  localPosts = JSON.parse(localStorage.getItem("posts"));
+  localPosts.forEach((post, postIndex) => {
+    posts.push(new Post(post.body, post.author, post.id));
+    post.comments.forEach((comment, commentIndex) => {
+      posts[postIndex].comments.push(new Comment(comment.body, comment.author, comment.id, comment.postID));
+    });
+  });
+}
+// posts.push(new Post("I'm not sure what to do with my life", "John", 1));
+// posts[1].comments = [new Comment("I agree", "John", 0, 1), new Comment("I disagree", "Jane", 1, 1), new Comment("I'm not sure", "Joe", 2, 1)];
 
 render();
